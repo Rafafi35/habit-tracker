71c4ac1ad13090e1088511746cb05a9ce6fdab52
Rafafi35 <salinger.rafael@gmail.com>
2025-11-12 16:11:22 +0100

    setup expo router

---
+++ app/index.jsx
@@ -0,0 +1,10 @@
+import { StyleSheet, Text, View, } from 'react-native';
+const Home = () => {
+  return (
+    <View>
+      <Text>Homescreen</Text>
+    </View>
+  );
+}
+export default Home;
+const styles = StyleSheet.create({})
