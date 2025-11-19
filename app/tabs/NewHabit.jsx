5d4b4acf468436c091f1a5c9a597d41707d04bd5
Rafafi35 <salinger.rafael@gmail.com>
2025-11-19 15:18:45 +0100

    add Router Tab

---
+++ app/tabs/NewHabit.jsx
@@ -0,0 +1,9 @@
+import { StyleSheet, Text, View, } from 'react-native';
+
+export default function NewHabit() {
+    return (
+        <View>
+            <Text>New Habit Screen</Text>
+        </View>
+    )
+}
